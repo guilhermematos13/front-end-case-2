2d66e2593e85d2e6349b257854fdab2202e60727
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-16 19:31:07 -0300

    feat: creating menu banner component

---
+++ src/components/MenuBanner.tsx
@@ -0,0 +1,19 @@
+import Image, { StaticImageData } from 'next/image';
+import { MenuButton } from './MenuButton';
+
+interface MenuBannerProps {
+  title: string;
+  image: StaticImageData;
+  description: string;
+}
+
+export function MenuBanner({ title, image, description }: MenuBannerProps) {
+  return (
+    <div className="w-1/3 p-6 flex flex-col items-center justify-center bg-gray-200 rounded-md">
+      <Image src={image} alt="" className="w-20 h-20" />
+      <strong className="text-2xl text-green-primary mt-6">{title}</strong>
+      <p className="mt-3 text-center mb-6">{description}</p>
+      <MenuButton title={`Detalhes ${title}`} />
+    </div>
+  );
+}
