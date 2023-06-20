9a62f5ebb3b1e61232dc710ea34eb0385594c60c
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-19 23:39:26 -0300

    feat: creating modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 export function Modal({ children, openModal, closeModal }: ModalProps) {
   return (
     <ModalMaterial open={openModal} onClose={closeModal}>
-      <Box className="fixed left-1/2 top-1/2 w-1/3 -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-blue-primary bg-slate-200/80 px-10 py-8 text-gray-950 shadow-lg shadow-black/25 outline-none">
+      <Box className="fixed left-1/2 top-1/2 w-1/3 -translate-x-1/2 -translate-y-1/2 rounded-lg border-2 border-blue-primary bg-slate-300 px-10 py-8 text-gray-950 shadow-lg shadow-black/25 outline-none">
         {children}
       </Box>
     </ModalMaterial>
