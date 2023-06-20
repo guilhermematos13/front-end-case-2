df5efd5f1f506062a15ca8d1e4106ffecdf5ee72
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-19 23:40:10 -0300

    feat: config modal open and close function and creating data for selects

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,19 +1,33 @@
 'use client'
 import { User } from '@phosphor-icons/react'
 import { Button } from '../../components/Button'
+import { useState } from 'react'
+import { ModalClient } from '../../partials/client/ModalClient'
 
 export default function ClientsPage() {
+  const [openModal, setOpenModal] = useState(false)
+
+  function handleChangeModal() {
+    setOpenModal((prev) => !prev)
+  }
+
   return (
-    <div className="mt-20 flex h-screen w-screen flex-col items-center px-48">
-      <div className="mb-8 flex w-full justify-between">
-        <h1 className="text-3xl font-bold text-blue-primary">Clientes</h1>
+    <div className="mt-20 flex h-screen flex-col items-center px-48 xs:px-4">
+      <div className="mb-6 flex w-full items-center justify-between">
+        <h1 className="font-bold text-blue-primary xs:text-base md:text-xl lg:text-3xl">
+          Clientes
+        </h1>
         <div>
           <Button
             icon={<User size={20} weight="fill" />}
             title="Criar um novo Cliente"
+            onClick={handleChangeModal}
           />
         </div>
       </div>
+      <div className="w-full border border-blue-primary/50" />
+
+      <ModalClient handleCloseModal={handleChangeModal} openModal={openModal} />
     </div>
   )
 }
---
+++ src/data/SelectDocumentData.ts
@@ -0,0 +1,16 @@
+interface SelectDocumentDataProps {
+  label: string
+  value: string
+}
+
+export const SelectDocumentData: SelectDocumentDataProps[] = [
+  {
+    label: 'CPF - Cadastro de Pessoas Físicas',
+    value: 'CPF',
+  },
+
+  {
+    label: 'RG - Registro Geral',
+    value: 'RG',
+  },
+]
