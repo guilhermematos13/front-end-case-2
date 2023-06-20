8611c494cf8c454072c8a878537c62df5a2c02f4
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-20 16:31:52 -0300

    feat: creating table client data

---
+++ src/data/TableClientsData.ts
@@ -0,0 +1,35 @@
+interface TableClientsDataProps {
+  label: string
+}
+
+export const TableClientsData: TableClientsDataProps[] = [
+  {
+    label: 'Documento',
+  },
+
+  {
+    label: 'N° do Documento',
+  },
+  {
+    label: 'Nome',
+  },
+  {
+    label: 'Logradouro',
+  },
+  {
+    label: 'Número',
+  },
+  {
+    label: 'UF',
+  },
+  {
+    label: 'Cidade',
+  },
+  {
+    label: 'Bairro',
+  },
+
+  {
+    label: 'Ações',
+  },
+]
